c0318246a057afd992e97920be87821f62628590
MarkKithinji <105651925+MarkKithinji@users.noreply.github.com>
2022-10-24 14:30:08 +0300

    Add project files.

---
+++ HelloWorld(II)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld_II_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "MarkKithinji";
+            int makeUnique = 2221;
+            string mailServer = ("@outlook");
+            string domain = (".com");
+            var Email = (name+makeUnique+mailServer+domain);
+            Console.WriteLine(Email);
+        }
+    }
+}
